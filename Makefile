5a1109c829d635ef40f4606dd24d829ec4708a1b
Krzysztof Karpowicz <christoph.karpowicz@gmail.com>
2026-02-15 22:23:07 +0100

    Add a simple charging driver and modify the Makefile so that the compiler would use the bin dir.

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 main:
-	avr-gcc -g -Os -mmcu=attiny13a -c display.c
-	avr-gcc -g -Os -mmcu=attiny13a -c main.c
-	avr-gcc -g -mmcu=attiny13a -o main.elf main.o display.o
-	avr-objcopy -j .text -j .data -O ihex main.elf main.hex
-	avrdude -c usbasp -p t13 -P usb -U flash:w:main.hex
+	avr-gcc -g -Os -mmcu=attiny13a -c display.c -o bin/display.o
+	avr-gcc -g -Os -mmcu=attiny13a -c main.c -o bin/main.o
+	avr-gcc -g -mmcu=attiny13a -o bin/main.elf bin/main.o bin/display.o
+	avr-objcopy -j .text -j .data -O ihex bin/main.elf bin/main.hex
+	avrdude -c usbasp -p t13 -P usb -U flash:w:bin/main.hex
--- main.c
+++ main.c
@@ -6,13 +6,13 @@
 void init() {
     // init display
     DDRB |= _BV(SHIFT_REG_SCK_PIN) | _BV(SHIFT_REG_RCK_PIN) | _BV(SHIFT_REG_SERIAL_PIN);
+    // init driver
+    DDRB |= _BV(PB3);
 
     // enable ADC with 64 prescaler division factor
     ADCSRA |= _BV(ADPS1) | _BV(ADPS2) | _BV(ADEN);
     // disable ADC
     // ADCSRA &= ~_BV(ADEN);
-
-    // PORTB |= _BV(PB3);
 }
 
 uint16_t adc_read() {
@@ -25,6 +25,14 @@ uint16_t adc_read() {
     return ADC;
 }
 
+void charging_on() {
+    PORTB |= _BV(PB3);
+}
+
+void charging_off() {
+    PORTB &= ~_BV(PB3);
+}
+
 int main(void) {
     init();
     while (1) {
@@ -32,26 +40,37 @@ int main(void) {
         uint16_t adc_result = adc_read();
         if (adc_result >= 1000){
             display_number(5, 0);
+            charging_on();
         } else if (adc_result >= 921) {
             display_number(4, 1);
+            charging_on();
         } else if (adc_result >= 820) {
             display_number(4, 0);
+            charging_on();
         } else if (adc_result >= 716) {
             display_number(3, 1);
+            charging_off();
         } else if (adc_result >= 615) {
             display_number(3, 0);
+            charging_off();
         } else if (adc_result >= 512) {
             display_number(2, 1);
+            charging_off();
         } else if (adc_result >= 410) {
             display_number(2, 0);
+            charging_off();
         } else if (adc_result >= 307) {
             display_number(1, 1);
+            charging_off();
         } else if (adc_result >= 205) {
             display_number(1, 0);
+            charging_off();
         } else if (adc_result >= 102) {
             display_number(0, 1);
+            charging_off();
         } else {
             display_number(0, 0);
+            charging_off();
         }
     }
     return 0;
